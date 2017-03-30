116dc34b9bdfe401e3cc8630841c5f1610a8603b
Andres Weber <andresmweber@gmail.com>
2017-03-30 16:52:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='MPCSave',
+    name='SavePlus+',
     version='0.0.1',
     description='Save versioning of files in Maya',
     long_description=readme,
     author='Andres Weber',
     author_email='andresmweber@gmail.com',
-    url='https://github.com/andresmweber/mpcsave',
+    url='https://github.com/andresmweber/SavePlus-',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     test_suite='nose.collector',
